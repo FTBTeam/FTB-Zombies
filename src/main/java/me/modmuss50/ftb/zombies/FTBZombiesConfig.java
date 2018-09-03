fe0aebae481ce71f3e667640840c8afaa7696908
LatvianModder <latvianmodder@gmail.com>
2018-09-03 22:50:04 +0300

    Added model and name for controller block, sound playback, randomized zombie names, config for radius, fixed radius bug, fixed villager duplication bug, replaced dynamic cell item checker with ObjectHolder

---
+++ src/main/java/me/modmuss50/ftb/zombies/FTBZombiesConfig.java
@@ -0,0 +1,21 @@
+package me.modmuss50.ftb.zombies;
+
+import net.minecraftforge.common.config.Config;
+
+@Config(modid = FTBZombies.MOD_ID, category = "")
+public class FTBZombiesConfig {
+	public static final General general = new General();
+
+	public static class General {
+		public int controller_radius = 21;
+		public int controller_height = 22;
+		public int zombie_count = 10;
+		public String login = "minecraft:entity.parrot.fly";
+		public String zombie_sound = "minecraft:entity.enderdragon.growl";
+		public String villager_sound = "minecraft:ui.toast.challenge_complete";
+		public String[] names = {
+				"Dr. Zombie",
+				"Mister Villager"
+		};
+	}
+}
--- src/main/java/me/modmuss50/ftb/zombies/TileController.java
+++ src/main/java/me/modmuss50/ftb/zombies/TileController.java
@@ -8,10 +8,17 @@ import net.minecraft.entity.ai.EntityAITasks;
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.monster.EntityZombieVillager;
 import net.minecraft.entity.passive.EntityVillager;
+import net.minecraft.init.Items;
 import net.minecraft.init.MobEffects;
+import net.minecraft.item.Item;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.potion.PotionEffect;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.SoundEvent;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraftforge.fml.common.network.NetworkRegistry;
+import net.minecraftforge.fml.common.registry.GameRegistry;
 import reborncore.common.network.NetworkManager;
 import reborncore.common.tile.TileLegacyMachineBase;
 
@@ -19,6 +26,26 @@ import java.util.List;
 import java.util.function.Predicate;
 
 public class TileController extends TileLegacyMachineBase {
+	@GameRegistry.ObjectHolder("techreborn:dynamiccell")
+	public static Item DYNAMICCELL = Items.AIR;
+
+	public int topZombieCount = 0;
+	public int topVillagerCount = 0;
+
+	@Override
+	public void readFromNBT(NBTTagCompound nbt) {
+		super.readFromNBT(nbt);
+		topZombieCount = nbt.getInteger("ZombieCount");
+		topVillagerCount = nbt.getInteger("VillagerCount");
+	}
+
+	@Override
+	public NBTTagCompound writeToNBT(NBTTagCompound nbt) {
+		nbt = super.writeToNBT(nbt);
+		nbt.setInteger("ZombieCount", topZombieCount);
+		nbt.setInteger("VillagerCount", topVillagerCount);
+		return nbt;
+	}
 
 	@Override
 	public void update() {
@@ -28,53 +55,91 @@ public class TileController extends TileLegacyMachineBase {
 			return;
 		}
 
-		double size = 21;
-		AxisAlignedBB bb = new AxisAlignedBB(getPos().getX() - size, getPos().getY(), getPos().getZ() - size, getPos().getX() + size, getPos().getY() + size, getPos().getZ() + size);
+		double size = FTBZombiesConfig.general.controller_radius;
+		AxisAlignedBB bb = new AxisAlignedBB(getPos().getX() - size, getPos().getY(), getPos().getZ() - size, getPos().getX() + size + 1, getPos().getY() + FTBZombiesConfig.general.controller_height, getPos().getZ() + size + 1);
 		List<EntityZombieVillager> zombies = world.getEntitiesWithinAABB(EntityZombieVillager.class, bb);
 		List<EntityVillager> villagers = world.getEntitiesWithinAABB(EntityVillager.class, bb);
-		List<EntityItem> items = world.getEntitiesWithinAABB(EntityItem.class, bb);
 
 		//stops the zombies attacking the villagers
 		zombies.forEach(entityZombieVillager -> removeTask(entityZombieVillager, entityAIBase -> entityAIBase instanceof EntityAINearestAttackableTarget));
 		//Stops the villagers from being scared of the zombies
 		villagers.forEach(entityZombieVillager -> removeTask(entityZombieVillager, entityAIBase -> entityAIBase instanceof EntityAIAvoidEntity));
 
+		if(topVillagerCount != -1) {
+			int prev = topVillagerCount;
+			topVillagerCount = Math.max(topVillagerCount, villagers.size());
+
+			if (prev < topVillagerCount && topVillagerCount >= FTBZombiesConfig.general.zombie_count) {
+				SoundEvent event = SoundEvent.REGISTRY.getObject(new ResourceLocation(FTBZombiesConfig.general.villager_sound));
+				if(event != null) {
+					world.playSound(null, getPos(), event, SoundCategory.BLOCKS, 8F, 1F);
+				}
+				topVillagerCount = -1;
+			}
+
+			if(prev != topVillagerCount){
+				markDirty();
+			}
+		}
 
 		if (zombies.isEmpty()) {
 			return;
+		}
+
+		if(topZombieCount != -1) {
+			int prev = topZombieCount;
+			topZombieCount = Math.max(topZombieCount, zombies.size());
+
+			if (prev < topZombieCount && topZombieCount >= FTBZombiesConfig.general.zombie_count) {
+				SoundEvent event = SoundEvent.REGISTRY.getObject(new ResourceLocation(FTBZombiesConfig.general.zombie_sound));
+				if(event != null) {
+					world.playSound(null, getPos(), event, SoundCategory.BLOCKS, 8F, 1F);
+				}
+				topZombieCount = -1;
+			}
+
+			if(prev != topZombieCount){
+				markDirty();
+			}
 		}
 
 		for(EntityZombieVillager zombieVillager : zombies) {
 			zombieVillager.addPotionEffect(new PotionEffect(MobEffects.GLOWING, 60));
 			zombieVillager.enablePersistence();
 			zombieVillager.setAlwaysRenderNameTag(true);
+			zombieVillager.setProfession(1);
+
+			if(!zombieVillager.hasCustomName()) {
+				zombieVillager.setCustomNameTag(FTBZombiesConfig.general.names[world.rand.nextInt(FTBZombiesConfig.general.names.length)]);
+			}
 		}
 
-		if (items.isEmpty()) {
-			return;
-		}
+		for (EntityItem item : world.getEntitiesWithinAABB(EntityItem.class, bb)) {
+			if (item.getItem().getItem() == DYNAMICCELL) {
+				EntityZombieVillager zombieVillager = zombies.get(0);
 
-		for (EntityItem item : items) {
-			if (!item.getItem().getItem().getRegistryName().toString().equals("techreborn:dynamiccell")) {
-				continue;
+				EntityVillager newVillager = new EntityVillager(zombieVillager.world);
+				newVillager.setProfession(zombieVillager.getForgeProfession());
+				newVillager.setLocationAndAngles(zombieVillager.posX, zombieVillager.posY, zombieVillager.posZ, zombieVillager.rotationYaw, zombieVillager.rotationPitch);
+				if (zombieVillager.hasCustomName())
+				{
+					newVillager.setCustomNameTag(zombieVillager.getCustomNameTag());
+					newVillager.setAlwaysRenderNameTag(true);
+				}
+				world.spawnEntity(newVillager);
+				newVillager.enablePersistence();
+
+				item.getItem().shrink(1);
+
+				if(item.getItem().isEmpty()) {
+					item.setDead();
+				}
+
+				zombieVillager.setDead();
+
+				NetworkManager.sendToAllAround(new PacketSpawnSmoke(newVillager.getPos()), new NetworkRegistry.TargetPoint(world.provider.getDimension(), newVillager.posX, newVillager.posY, newVillager.posZ, 64));
+				break;
 			}
-			EntityZombieVillager zombieVillager = zombies.get(0);
-
-			EntityVillager newVillager = new EntityVillager(zombieVillager.world);
-			newVillager.setProfession(zombieVillager.getForgeProfession());
-			newVillager.setLocationAndAngles(zombieVillager.posX, zombieVillager.posY, zombieVillager.posZ, zombieVillager.rotationYaw, zombieVillager.rotationPitch);
-			if(zombieVillager.hasCustomName()){
-				newVillager.setCustomNameTag(zombieVillager.getCustomNameTag());
-				newVillager.setAlwaysRenderNameTag(true);
-			}
-			world.spawnEntity(newVillager);
-			newVillager.enablePersistence();
-
-			item.setDead();
-			zombieVillager.setDead();
-
-			NetworkManager.sendToAllAround(new PacketSpawnSmoke(newVillager.getPos()), new NetworkRegistry.TargetPoint(world.provider.getDimension(), newVillager.posX, newVillager.posY, newVillager.posZ, 64));
-
 		}
 	}
 
@@ -89,7 +154,5 @@ public class TileController extends TileLegacyMachineBase {
 		if (aiBase != null) {
 			entityLiving.targetTasks.removeTask(aiBase);
 		}
-
 	}
-
 }
